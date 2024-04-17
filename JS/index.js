a0a73360f1bbaf323e140e996ded59133e1e85cf
DMU-P2589360 <92032248+DMU-P2589360@users.noreply.github.com>
2024-04-17 15:08:13 +0100

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+new hoverEffect ({
+    parent: document.querySelector ('.distortion'),
+    intensity:0.2,
+    image1: "./Images/Meet the Team/Lady2.png",
+    image2: "./Images/Meet the Team/Lady3.png",
+    displacementImage: "./dis.png",
+})
